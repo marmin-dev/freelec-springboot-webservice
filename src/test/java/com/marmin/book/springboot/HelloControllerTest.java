f39a660c0cf00f74512d538fdc3d883939d27093
Marmin <marmin.dev@gmail.com>
2022-09-10 01:20:03 +0900

    Implements lombok and add Dto controller and Unittest

--- src/test/java/com/marmin/book/springboot/HelloControllerTest.java
+++ src/test/java/com/marmin/book/springboot/HelloControllerTest.java
@@ -8,11 +8,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class)
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+@ExtendWith(SpringExtension.class)//테스트를 진행할때 Junit에 내장된 실행자 외에 다른 실행자를 실행시킨다
+//스프링 부트 테스트와 JUnit사이의 연결자 역활을 한다
+@WebMvcTest(controllers = HelloController.class)//
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -22,6 +22,20 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name",name)
+                        .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 
 }

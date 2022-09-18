4000d0e4d1d3f9cfa982a04e3870f0cafbe2926c
Marmin <marmin.dev@gmail.com>
2022-09-18 15:38:20 +0900

    fix error 404 main page

--- src/main/java/com/marmin/book/springboot/web/IndexController.java
+++ src/main/java/com/marmin/book/springboot/web/IndexController.java
@@ -1,13 +1,15 @@
 package com.marmin.book.springboot.web;
 
+import com.sun.org.apache.xml.internal.security.Init;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public  String index(){
         return "index";
     }
 }

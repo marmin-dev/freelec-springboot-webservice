fdc289cc0fd0400ba05eb9a865175daca827ff5f
Marmin <marmin.dev@gmail.com>
2022-10-01 19:18:14 +0900

    add modify function and need to fix 500 error

--- src/main/java/com/marmin/book/springboot/web/IndexController.java
+++ src/main/java/com/marmin/book/springboot/web/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUdate(@PathVariable Long id,Model model){
+    public String postsUpdate(@PathVariable Long id,Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post",dto);
         return "posts-update";

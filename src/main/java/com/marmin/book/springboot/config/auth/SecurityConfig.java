8da5768e8a0bd306a27f3fa23a272e64e871148b
Marmin <marmin.dev@gmail.com>
2022-10-07 03:17:30 +0900

    Made LoginUser annotation interface

--- src/main/java/com/marmin/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/marmin/book/springboot/config/auth/SecurityConfig.java
@@ -36,7 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2Login()//OAuth2 로그인 기능에 대한 여러 설정의 진입점입니다
                 .userInfoEndpoint()//OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당합니다
-                .userService(customOauth2UserService); //소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의
+                .userService(customOauth2UserService)
+                ;
+                 //소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의
         //구현체를 등록합니다
         //리소스 서버(즉,소셜 서비스들)에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시할수 있습니다
 
